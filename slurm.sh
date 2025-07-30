e62d1348d0cc78e2ef1f6e6c512401f543b567e7
joseph <jmcinern@tcd.ie>
2025-07-30 18:19:38 +0100

    added 256 CPUs just to see

--- slurm.sh
+++ slurm.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=oireachtas_XML_collect
 #SBATCH --output=./out/oireachtas_XML_collect%j.out
 #SBATCH --error=./err/oireachtas_XML_collect_%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:15:00
 #SBATCH --partition=k2-lowpri  # Changed from k2-gpu-v100
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=256
 #SBATCH --mem=16G  # Reduced from 256G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=josephmcinerney7575@gmail.com
